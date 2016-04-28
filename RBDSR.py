bbea9c7e003ae2fa8da289e9e289826855f35953
zhoubofsy <zhoubofsy@hotmail.com>
2016-04-28 16:11:11 +0800

    格式化缩进

--- RBDSR.py
+++ RBDSR.py
@@ -57,14 +57,14 @@ def dbg_prt(fmt, *arg):
 '''
 class RBDSR(ISCSISR.ISCSISR):
     def handles(type):
-	dbg_prt("[rbdsr] handles")
+        dbg_prt("[rbdsr] handles")
         if type == "rbd":
             return True
         return False
     handles = staticmethod(handles)
 
     def load(self, sr_uuid):
-	dbg_prt("[rbdsr] load")
+        dbg_prt("[rbdsr] load")
         ''' Some repetition since LVHDoISCSI has pretty much everything inside of load method'''
         # Check if minimal amount of parameters(i.e. IP address of monitor) is passed to the call
         if not self.dconf.has_key('target') or not self.dconf['target']:
@@ -83,22 +83,22 @@ class RBDSR(ISCSISR.ISCSISR):
         ''' During XC SR creation, dialog first dicovers IQN and only then LUN.
         So if we have targetIQN in device-config but no SCSIid, means we discovering LUN/RBD image here '''
         if self.dconf.has_key('targetIQN') and not self.dconf.has_key('SCSIid'):
-	    dbg_prt("[rbdsr] targetIQN: [%s]", self.dconf['targetIQN'])
+            dbg_prt("[rbdsr] targetIQN: [%s]", self.dconf['targetIQN'])
             pool_name = self.dconf['targetIQN']
             
             ### Getting RBD image corresponding to RBD pool
             block_list = self._getCEPH_response('rbd -p %s ls' % pool_name)
-	    block_list.pop() # pop last one which is a blank
+            block_list.pop() # pop last one which is a blank
             rbd_image_list = self._formatRBD_image(pool_name, block_list)
             dbg_prt("[rbdsr] rbd ls, poolname:[%s], block_list:[%s], rbd_image_list:[%s]", pool_name, block_list, rbd_image_list)
             
-	    ''' We don't attach rbd during discovery of the images, 
+            ''' We don't attach rbd during discovery of the images, 
             but parent class(LVHDoISCSI) needs 'attached' flag to print LUNs'''
-	    self.attached = True
+            self.attached = True
             
             ''' If we have SCSIid, means we have discovered targetIQN already and ready to attach rbd block'''    
         elif self.dconf.has_key('SCSIid'):
-	    dbg_prt("[rbdsr] SCSIid begin:")
+            dbg_prt("[rbdsr] SCSIid begin:")
             self.attached = os.path.exists('/dev/disk/by-scsid/%s' % self.dconf['SCSIid'])
             self.path = '/dev/disk/by-id/scsi-%s' % self.dconf['SCSIid']
             if os.path.exists('/var/lock/sm/%s/sr' % sr_uuid) and not self.attached:
@@ -108,17 +108,17 @@ class RBDSR(ISCSISR.ISCSISR):
             either targetIQN nor SCSIid, which means we are at the begining of the XC iSCSI SR create dialog
             and need to discover RBD pools from the monitors.'''
         else:
-	    dbg_prt("[rbdsr] Get RBD pool :")
+            dbg_prt("[rbdsr] Get RBD pool :")
             ### Getting RBD pool using ssh user and password
             rbd_pool_string = self._getCEPH_response('ceph osd lspools')
-	    rbd_pool_string.pop() # remove last one which is a blank.
+            rbd_pool_string.pop() # remove last one which is a blank.
             if 'fault' in rbd_pool_string or not rbd_pool_string:
                 raise xs_errors.XenError('ISCSILogin')
             else:
                 self._cleanCEPH_folder('/var/lib/rbd')
                 rbd_pool_list = self._formatRBD_pool(rbd_pool_string)
 
-	    dbg_prt("[rbdsr] rbd_pool_string : %s, rbd_pool_list : %s]", rbd_pool_string, rbd_pool_list)
+            dbg_prt("[rbdsr] rbd_pool_string : %s, rbd_pool_list : %s]", rbd_pool_string, rbd_pool_list)
             '''Discovered list should look like this: ('<ip address>:<port>', '<tgt>', '<iqn>')'''
             map = []
             for pool in rbd_pool_list.split('\n'):
@@ -135,11 +135,11 @@ class RBDSR(ISCSISR.ISCSISR):
             raise xs_errors.XenError('ConfigTargetIQNMissing')
 
             self.targetIQN = unicode(self.dconf['targetIQN']).encode('utf-8')
-	    dbg_prt("[rbdsr] targetIQN: %s", self.dconf['targetIQN'])
+            dbg_prt("[rbdsr] targetIQN: %s", self.dconf['targetIQN'])
             
             
     def _formatCEPH_key(self, auth_key_string):
-	dbg_prt("[rbdsr] _formatCEPH_key")
+        dbg_prt("[rbdsr] _formatCEPH_key")
         if not os.path.exists('/etc/rbd'):
             os.makedirs('/etc/rbd')
         a = open('/etc/rbd/auth', 'w')
@@ -152,7 +152,7 @@ class RBDSR(ISCSISR.ISCSISR):
         
         
     def _formatMON_list(self, mon_addrs_xml):
-	dbg_prt("[rbdsr] _formatMON_list")
+        dbg_prt("[rbdsr] _formatMON_list")
         addrs_xml = parseString(mon_addrs_xml[0].rstrip())
         if not os.path.exists('/etc/rbd'):
             os.makedirs('/etc/rbd')
@@ -167,17 +167,17 @@ class RBDSR(ISCSISR.ISCSISR):
         
         
     def _formatRBD_pool(self, rbd_pool_string):
-	dbg_prt("[rbdsr] _formatRBD_pool rbd_pool_string:%s, typ:%s", rbd_pool_string, type(rbd_pool_string))
+        dbg_prt("[rbdsr] _formatRBD_pool rbd_pool_string:%s, typ:%s", rbd_pool_string, type(rbd_pool_string))
         base_path = '/var/lib/rbd/'
         pool_name_list = ''
-	first_item = True
+        first_item = True
         for pool in rbd_pool_string[0].split(','):
-	    pool.rstrip()
+            pool.rstrip()
             if not pool.isspace() and pool != '':
-		# single pool no need '\n'
-		if not first_item:
-		    pool_name_list += '\n'
-		first_item = False
+                # single pool no need '\n'
+                if not first_item:
+                    pool_name_list += '\n'
+                first_item = False
                 pool_name = (pool.split(' ')[1])
                 pool_name_list += pool_name
                 pool_path = os.path.join(base_path,pool_name)
@@ -187,14 +187,14 @@ class RBDSR(ISCSISR.ISCSISR):
         
         
     def _formatRBD_image(self, rbd_pool_name, rbd_image_string):
-	dbg_prt("[rbdsr] _formatRBD_image")
-	image_arry = []
+        dbg_prt("[rbdsr] _formatRBD_image")
+        image_arry = []
         base_path = '/var/lib/rbd/'
         pool_path = os.path.join(base_path, rbd_pool_name)
         self._cleanCEPH_folder(pool_path,False)
-	dbg_prt("[rbdsr] formatRBD_image rbd_image_string:[%s]", rbd_image_string)
+        dbg_prt("[rbdsr] formatRBD_image rbd_image_string:[%s]", rbd_image_string)
         for block in rbd_image_string:
-	    if not block.isspace() and block != '':
+            if not block.isspace() and block != '':
                 rbd_image_path = os.path.join(base_path, rbd_pool_name, block.rstrip())
                 rbd_image = open(rbd_image_path,'w')
                 image_info_response = self._getCEPH_response('rbd --format json -p %s info %s' % (rbd_pool_name, block))
@@ -206,14 +206,14 @@ class RBDSR(ISCSISR.ISCSISR):
                 util.SMlog('RBD info of the image is %s' % image_info)
                 rbd_image.write('%s' % image_info)
                 rbd_image.close()
-		image_arry.append(block.rstrip())
-		dbg_prt("[rbdsr] formatRBD_image rbd_image_path:[%s], image_info_response:[%s], image_info:[%s]", rbd_image_path, image_info_response, image_info)
-	dbg_prt("[rbdsr] formatRBD_image image_arry : %s", image_arry)
+                image_arry.append(block.rstrip())
+                dbg_prt("[rbdsr] formatRBD_image rbd_image_path:[%s], image_info_response:[%s], image_info:[%s]", rbd_image_path, image_info_response, image_info)
+        dbg_prt("[rbdsr] formatRBD_image image_arry : %s", image_arry)
         return image_arry
         
         
     def _cleanCEPH_folder(self, path,remove_root=True):
-	dbg_prt("[rbdsr] _cleanCEPH_folder")
+        dbg_prt("[rbdsr] _cleanCEPH_folder")
         if os.path.exists(path):
             for root, dirs, files in os.walk(path, topdown=False):
                 for name in files:
@@ -224,7 +224,7 @@ class RBDSR(ISCSISR.ISCSISR):
                 os.rmdir(root)
                 
     def _getRBD_index(self, image_name):
-	dbg_prt("[rbdsr] _getRBD_index")
+        dbg_prt("[rbdsr] _getRBD_index")
         parent_folder = ''
         for root, dirs, files in os.walk('/sys/devices/rbd', topdown=False):
             current_folder = root.split('/')[-1]
@@ -240,7 +240,7 @@ class RBDSR(ISCSISR.ISCSISR):
         
         
     def _getCEPH_response(self, cmd):
-	dbg_prt("[rbdsr] _getCEPH_response")
+        dbg_prt("[rbdsr] _getCEPH_response")
         s = pxssh.pxssh()
         s.force_password = True
         password = ""
@@ -257,8 +257,8 @@ class RBDSR(ISCSISR.ISCSISR):
             s.sendline (cmd)
             s.prompt()
             result = s.before.split('\n')
-	    # remove the last one which is a blank
-	    dbg_prt("[rbdsr] ceph_response cmd : [%s], result : [%s], result_len : %d", cmd, result, len(result))
+            # remove the last one which is a blank
+            dbg_prt("[rbdsr] ceph_response cmd : [%s], result : [%s], result_len : %d", cmd, result, len(result))
             return result[1:]
                 
                 
@@ -314,46 +314,46 @@ class RBDSR(ISCSISR.ISCSISR):
         else:
             '''in iSCSI sr we need to attach target to interrogate LUN for size, scsi_id etc etc. 
             We don't need to do this with current way of finding things over ssh'''
-	    dbg_prt("[rbdsr] attach Has no key of SCSIid")
+            dbg_prt("[rbdsr] attach Has no key of SCSIid")
             self.attached = True
       
       
     def detach(self, sr_uuid):
-	dbg_prt("[rbdsr] detach")
+        dbg_prt("[rbdsr] detach")
         if self.dconf.has_key('SCSIid') and self.dconf['SCSIid']:
             rbd_image_name = self.dconf['SCSIid']
             rbd_disk_path =  '/dev/disk/by-id/scsi-%s' % rbd_image_name
             rbd_scsi_path =  '/dev/disk/by-scsid/%s' % rbd_image_name
             rbd_image_index = self._getRBD_index(rbd_image_name)
             if os.path.exists(rbd_disk_path):
-		dbg_prt("[rbdsr] unlink rbd_disk_path:%s",rbd_disk_path)
+                dbg_prt("[rbdsr] unlink rbd_disk_path:%s",rbd_disk_path)
                 os.unlink(rbd_disk_path)
             if os.path.exists(rbd_scsi_path):
-		dbg_prt("[rbdsr] cleanCEPH_folder:%s",rbd_scsi_path)
+                dbg_prt("[rbdsr] cleanCEPH_folder:%s",rbd_scsi_path)
                 self._cleanCEPH_folder(rbd_scsi_path)
             if os.path.exists('/dev/rbd%s' % rbd_image_index):
-		dbg_prt("[rbdsr] remove rbd device idx:%s",rbd_image_index)
+                dbg_prt("[rbdsr] remove rbd device idx:%s",rbd_image_index)
                 rbd_remove = open('/sys/bus/rbd/remove','w')
                 rbd_remove.write(rbd_image_index)
                 rbd_remove.close()
-	else:
-	    dbg_prt("[rbdsr] detach do nothing !")
+        else:
+            dbg_prt("[rbdsr] detach do nothing !")
         self.attached = False
       
       
     def refresh(self):
-	dbg_prt("[rbdsr] refresh")
+        dbg_prt("[rbdsr] refresh")
         # Unlike iSCSI SR we don't need to refresh paths or rescan sessions
         pass
 
     def print_LUNs(self):
-	dbg_prt("[rbdsr] PRT_LUNS begin ===>")
+        dbg_prt("[rbdsr] PRT_LUNS begin ===>")
         self.LUNs = {}
         rbd_size = ''
         rbd_identity = ''
-	counter = 0
+        counter = 0
         pool_path = os.path.join('/var/lib/rbd', self.dconf['targetIQN'])
-	dbg_prt("[rbdsr] PRT_LUNS pool_path:[%s]", pool_path)
+        dbg_prt("[rbdsr] PRT_LUNS pool_path:[%s]", pool_path)
         for file in util.listdir(pool_path):
             lun_path = os.path.join(pool_path, file)
             lun_file = open(lun_path, 'rt')
@@ -366,15 +366,15 @@ class RBDSR(ISCSISR.ISCSISR):
             if rbd_size and rbd_identity:
                 obj = self.vdi(self.uuid)
                 self._divert_query(obj, lun_path,rbd_identity,rbd_size,file, counter)
-		counter += 1
+                counter += 1
                 self.LUNs[obj.uuid] = obj
-	dbg_prt("[rbdsr] PRT_LUNS end <===")
+        dbg_prt("[rbdsr] PRT_LUNS end <===")
     
     def _format_uuid_from_string(self, uuid):
-	return uuid[0:8] + "-" + uuid[8:12] + "-" + uuid[12:16] + "-" + uuid[16:20] + "-" + uuid[20:32]
+        return uuid[0:8] + "-" + uuid[8:12] + "-" + uuid[12:16] + "-" + uuid[16:20] + "-" + uuid[20:32]
 
     def _divert_query(self, vdi, path, rbd_id, rbd_size, lun_name, count):
-	dbg_prt("[rbdsr] _divert_query, rbd_id:%s, rbd_size:%s, lun_name:%s", rbd_id, rbd_size, lun_name )
+        dbg_prt("[rbdsr] _divert_query, rbd_id:%s, rbd_size:%s, lun_name:%s", rbd_id, rbd_size, lun_name )
         vdi.uuid = scsiutil.gen_uuid_from_string(rbd_id)
         vdi.location = self.uuid
         vdi.vendor = 'RADOS'
@@ -387,12 +387,12 @@ class RBDSR(ISCSISR.ISCSISR):
         sm_config['LUNid'] = vdi.LUNid
         sm_config['SCSIid'] = vdi.SCSIid
         vdi.sm_config = sm_config
-	dbg_prt("[rbdsr] vdi[uuid:%s,location:%s,vendor:%s,serial:%s,LUNid:%s,size:%s,SCSIid:%s,path:%s]", vdi.uuid, vdi.location,vdi.vendor,vdi.serial,vdi.LUNid,vdi.size,vdi.SCSIid,vdi.path)
-	dbg_prt("[rbdsr] sm_config[%s]", sm_config)
+        dbg_prt("[rbdsr] vdi[uuid:%s,location:%s,vendor:%s,serial:%s,LUNid:%s,size:%s,SCSIid:%s,path:%s]", vdi.uuid, vdi.location,vdi.vendor,vdi.serial,vdi.LUNid,vdi.size,vdi.SCSIid,vdi.path)
+        dbg_prt("[rbdsr] sm_config[%s]", sm_config)
       
       
     def _attach_LUN_bySCSIid(self, SCSIid):
-	dbg_prt("[rbdsr] _attach_LUN_bySCSIid")
+        dbg_prt("[rbdsr] _attach_LUN_bySCSIid")
         if os.path.exists('/dev/disk/by-id/scsi-%s' % SCSIid):
             return True
         else:
